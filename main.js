999accbd526d35d39236f848a33938203edaa45f
Elizabeth Hahn <elizhahn87@gmail.com>
2020-12-17 17:49:35 -0700

    Create display recipe bar functionality

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 var inputDishes = document.querySelectorAll("input[name='dish']");
 var btnLetsCook = document.querySelector(".lets-cook-btn");
+var btnAddRecipe = document.querySelector(".add-a-recipe");
 var imgCookPot = document.querySelector("svg");
 var randomDishIntro = document.querySelector("p");
 var randomDishText = document.querySelector(".random-dish");
+var recipeBar = document.querySelector(".add-recipe-bar");
 var randomDish;
 
 btnLetsCook.addEventListener("click", displayRandomDish);
+btnAddRecipe.addEventListener("click", displayRecipeBar);
+
 
 //retrieves random array index
 function randomIndex(array) {
@@ -35,4 +39,8 @@ function displayDish() {
   imgCookPot.classList.add("hidden");
   randomDishIntro.classList.remove("hidden");
   randomDishText.innerText = `${randomDish}!`
+}
+
+function displayRecipeBar() {
+  recipeBar.classList.remove("hidden");
 }
