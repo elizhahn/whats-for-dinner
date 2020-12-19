f6a33aa31eb2d88e3e8992b271a9a3ac38c92336
Elizabeth Hahn <elizhahn87@gmail.com>
2020-12-19 16:08:17 -0700

    Check for empty value in validation function

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 
-var inputDishes = document.querySelectorAll("input[name='dish']");
-var inputUserDish = document.getElementById("recipe-type");
+var inputMealType = document.querySelectorAll("input[name='dish']");
+var inputUserMealType = document.getElementById("recipe-type");
 var inputUserRecipe = document.getElementById("recipe-name");
 var btnLetsCook = document.querySelector(".lets-cook-btn");
 var btnAddRecipe = document.querySelector(".add-a-recipe");
 var btnAddNew = document.querySelector(".add-new-btn");
 var imgCookPot = document.querySelector("svg");
-var randomDishIntro = document.querySelector("p");
-var randomDishText = document.querySelector(".random-dish");
+var randomRecipeIntro = document.querySelector("p");
+var randomRecipeText = document.querySelector(".random-dish");
 var recipeBar = document.querySelector(".add-recipe-bar");
 var messageError = document.getElementById("error");
-var randomDish;
+var randomRecipe;
 
 btnLetsCook.addEventListener("click", displayRandomDish);
 btnAddRecipe.addEventListener("click", displayRecipeBar);
@@ -24,29 +24,29 @@ function randomIndex(recipies) {
 //generates a random dish based on user input
 function displayRandomDish() {
   var userChoice;
-  for(var i = 0; i < inputDishes.length; i++) {
-    if(inputDishes[i].checked) {
-    userChoice = inputDishes[i].value
+  for(var i = 0; i < inputMealType.length; i++) {
+    if(inputMealType[i].checked) {
+    userChoice = inputMealType[i].value
     }
   }
   if(userChoice === "sides") {
-    randomDish = sides[randomIndex(sides)];
+    randomRecipe = sides[randomIndex(sides)];
   }else if (userChoice === "main-dishes") {
-    randomDish = mainDishes[randomIndex(sides)];
+    randomRecipe = mainDishes[randomIndex(sides)];
   }else {
-    randomDish = desserts[randomIndex(sides)];
+    randomRecipe = desserts[randomIndex(sides)];
   }
-  displayDish()
+  displayRecipe()
 }
 
 function changeDisplay() {
   imgCookPot.classList.add("hidden");
-  randomDishIntro.classList.remove("hidden");
+  randomRecipeIntro.classList.remove("hidden");
 }
 
-function displayDish() {
+function displayRecipe() {
   changeDisplay();
-  randomDishText.innerText = `${randomDish}!`
+  randomRecipeText.innerText = `${randomRecipe}!`
 }
 
 function displayRecipeBar() {
@@ -55,16 +55,19 @@ function displayRecipeBar() {
 
 function displayUserRecipe() {
   changeDisplay();
-  randomDishText.innerText = `${inputUserRecipe.value}`;
+  randomRecipeText.innerText = `${inputUserRecipe.value}`;
 }
 
 function validateForm(e) {
- if(inputUserDish.value.toLowerCase() !== "side" && inputUserDish.value.toLowerCase() !== "main dish" && inputUserDish.value.toLowerCase() !== "dessert") {
+ if(inputUserMealType.value.toLowerCase() !== "side" && inputUserMealType.value.toLowerCase() !== "main dish" && inputUserMealType.value.toLowerCase() !== "dessert") {
    e.preventDefault();
    messageError.classList.remove("hidden");
    messageError.innerText = "Please enter: side, main dish, or dessert";
-   messageError.style.color = "red";
-} else {
+} else if(inputUserRecipe.value === "") {
+    e.preventDefault();
+    messageError.classList.remove("hidden")
+    messageError.innerText = "input required";
+}else {
   e.preventDefault();
   messageError.classList.add("hidden");
   displayUserRecipe();
